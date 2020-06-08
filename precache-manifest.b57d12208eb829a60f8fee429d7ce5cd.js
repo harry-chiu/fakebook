29c50d57dcac1f8f10e13d4d58de8a1ef5b1ecf6
harry-chiu <harrychiu1106@gmail.com>
2020-06-08 16:02:13 +0800

    Updates

---
+++ precache-manifest.b57d12208eb829a60f8fee429d7ce5cd.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bdb22fe7867b7fbd63c880f3a5decb8",
+    "url": "/fakebook/index.html"
+  },
+  {
+    "revision": "a22c00210b0a88aab94a",
+    "url": "/fakebook/static/css/main.54adefa6.chunk.css"
+  },
+  {
+    "revision": "eca4202e3aeb241152ab",
+    "url": "/fakebook/static/js/2.bb26041f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/fakebook/static/js/2.bb26041f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "791ee07f2975aa18b8ac",
+    "url": "/fakebook/static/js/3.3fbe7fd2.chunk.js"
+  },
+  {
+    "revision": "6dff0d2e526730274b0b",
+    "url": "/fakebook/static/js/4.0fc2b5b4.chunk.js"
+  },
+  {
+    "revision": "a22c00210b0a88aab94a",
+    "url": "/fakebook/static/js/main.07c91c0c.chunk.js"
+  },
+  {
+    "revision": "1df373af827f29e29979",
+    "url": "/fakebook/static/js/runtime-main.4878083f.js"
+  },
+  {
+    "revision": "611f6ec2dd2c8bd10800f4fd22ebc9b3",
+    "url": "/fakebook/static/media/intro.611f6ec2.png"
+  }
+]);
