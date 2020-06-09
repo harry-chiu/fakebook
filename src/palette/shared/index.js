9b73eaf881bc7b170859a94f939de226c0c4d98e
harry-chiu <harrychiu1106@gmail.com>
2020-06-09 13:01:06 +0800

    [Delete] remove tooltip component

--- src/palette/shared/index.js
+++ src/palette/shared/index.js
@@ -1,7 +1,5 @@
-import tooltip from './tooltip';
 import divider from './divider';
 
 export default {
-  tooltip: { ...tooltip },
   divider: { ...divider },
 };
--- src/palette/shared/tooltip.js
+++
@@ -1,6 +0,0 @@
-export default {
-  border: '#c0c0c0',
-  popover: {
-    background: 'white',
-  },
-};
