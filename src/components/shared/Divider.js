30acd854ab24b9fa7d838b448ee5235f189b3f04
harry-chiu <harrychiu1106@gmail.com>
2020-06-09 14:08:22 +0800

    [Update] fix typo

--- src/components/shared/Divider.js
+++ src/components/shared/Divider.js
@@ -2,6 +2,6 @@ import styled from 'styled-components';
 import palette from '../../palette';
 
 export default styled.div`
-  border-bottom: 1px solid ${palette.shared.divider};
+  border-bottom: 1px solid ${palette.shared.divider.border};
   margin: 8px 0;
 `;
