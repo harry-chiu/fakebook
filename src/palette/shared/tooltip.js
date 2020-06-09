b5a13ccb098eb36f0016cce552ccef1dff2d8316
harry-chiu <harrychiu1106@gmail.com>
2020-06-09 15:57:33 +0800

    [Add] tooltip palette

---
+++ src/palette/shared/tooltip.js
@@ -0,0 +1,13 @@
+export default {
+  button: {
+    background: '#4267b2',
+    border: '#4267b2',
+    hover: {
+      background: '#365899',
+    },
+  },
+  text: '#1c1e21',
+  children: {
+    color: '#606770',
+  },
+};
